ac2d1ec679b712678785c81d7de8b59d3d05dd9d
John Jesus <john.jesus@raytheon.com>
2016-03-30 10:46:41 -0500

    Initial Strip Chart.

---
+++ TryMeasStudioChart/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using NationalInstruments.Analysis;
+using NationalInstruments.Analysis.Conversion;
+using NationalInstruments.Analysis.Dsp;
+using NationalInstruments.Analysis.Dsp.Filters;
+using NationalInstruments.Analysis.Math;
+using NationalInstruments.Analysis.Monitoring;
+using NationalInstruments.Analysis.SignalGeneration;
+using NationalInstruments.Analysis.SpectralMeasurements;
+using NationalInstruments;
+using NationalInstruments.Controls;
+using NationalInstruments.Controls.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TryMeasStudioChart
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
+        }
+
+        public MainWindowViewModel ViewModel
+        {
+            get { return (MainWindowViewModel)DataContext; }
+        }
+    }
+}
---
+++ TryMeasStudioChart/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using NationalInstruments.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace TryMeasStudioChart
+{
+    public class MainWindowViewModel
+    {
+        private ChartCollection<double> _chartData;
+        public ChartCollection<double> ChartData
+        {
+            get { return _chartData; }
+            set { _chartData = value; }
+        }
+
+        private readonly DispatcherTimer timer = new DispatcherTimer();
+        private readonly Random rand = new Random();
+
+        public MainWindowViewModel()
+        {
+            _chartData = new ChartCollection<double>(100);
+            timer.Tick += OnTimerTick;
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Start();
+        }
+
+        void OnTimerTick(object sender, EventArgs e)
+        {
+            GenerateData();
+        }
+
+        private void GenerateData()
+        {
+            _chartData.Append(rand.NextDouble());
+        }
+    }
+}
